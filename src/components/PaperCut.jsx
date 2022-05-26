1c5901ad90681bbf1481b4effba5c95c1ee74223
ahmadsan <ahmadsan4p@gmail.com>
2022-05-26 14:44:13 +0800

    added props

--- src/components/PaperCut.jsx
+++ src/components/PaperCut.jsx
@@ -3,11 +3,11 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 
-export function PaperCut() {
+export function PaperCut({ props }) {
   return (
     <Grid item xs={3}>
       <Paper sx={{ height: '100px', width: '90px', margin: '10px', padding: '10px', backgroundColor: '#132939' }}>
-        <Typography sx={{ color: 'white', padding: '0' }}>Hello</Typography>
+        <Typography sx={{ color: 'white', padding: '0' }}>{props.name}</Typography>
       </Paper>
     </Grid>
   );
